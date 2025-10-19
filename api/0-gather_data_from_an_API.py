31c476145cacc0a3c2eed7fe4ca9db0b80f899c5
Ced <c.bigwihin@alustudent.com>
2025-10-19 21:57:51 +0200

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,8 +8,9 @@ import sys
 
 if __name__ == "__main__":
     emp_id = int(sys.argv[1])
-    user_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    user_url = f"{base_url}/{emp_id}"
+    todos_url = f"{base_url}/{emp_id}/todos"
 
     user = requests.get(user_url).json()
     todos = requests.get(todos_url).json()
@@ -18,7 +19,9 @@ if __name__ == "__main__":
     total_tasks = len(todos)
     done_tasks = [task for task in todos if task.get("completed")]
 
-    print(f"Employee {emp_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+    print(
+        f"Employee {emp_name} is done with tasks"
+        f"({len(done_tasks)}/{total_tasks}):"
+    )
     for task in done_tasks:
         print(f"\t {task.get('title')}")
-
